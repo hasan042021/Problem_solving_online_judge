04976e055ccd6e3499a4f3862c83a29ba4785b84
Hasan <hasan.dev04@gmail.com>
2023-10-19 21:55:47 +0600

    week03 day06 added

--- week-03/O_Remove_Two_Letters.cpp
+++ week-03/O_Remove_Two_Letters.cpp
@@ -9,8 +9,22 @@ int main()
     {
         int n;
         cin >> n;
-        string a;
-        cin >> a;
+        string s;
+        cin >> s;
+        vector<string> ans;
+        for (int i = 0; i < n - 1; i++)
+        {
+            string ss = s.substr(i, 2);
+            string b;
+            if (!ans.empty())
+                b = ans.back();
+            reverse(b.begin(), b.end());
+            if (ans.empty() || ((ans.back() != ss) && b != ss))
+            {
+                ans.push_back(ss);
+            }
+        }
+        cout << ans.size() << endl;
     }
     return 0;
 }
